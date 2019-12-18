99dcb662f855a4741ce9a3de743758b53a6d3fbd
BuildTools <unconfigured@null.spigotmc.org>
2019-12-18 17:38:29 -0300

    Finished adding 'read_only'

--- src/main/java/me/rektb/bettershulkerboxes/events/InvClickEvent.java
+++ src/main/java/me/rektb/bettershulkerboxes/events/InvClickEvent.java
@@ -7,6 +7,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
 
 public class InvClickEvent implements Listener {
 
@@ -23,11 +24,10 @@ public class InvClickEvent implements Listener {
     @EventHandler
     public void inventoryClickInShulkerbox(InventoryClickEvent e) {
         Player p = (Player) e.getWhoClicked();
-        p.sendMessage(e.getClick().toString());
-        p.sendMessage(e.getClickedInventory().getType().toString());
-        p.sendMessage(e.getAction().toString());
-        p.sendMessage(e.getAction().toString());
         if (!cfgi.cfg_read_only) {
+            return;
+        }
+        if (!e.getInventory().getType().equals(InventoryType.SHULKER_BOX)) {
             return;
         }
         if (!shlkm.isHoldingShulker(p.getInventory().getItemInMainHand())) {
@@ -36,6 +36,7 @@ public class InvClickEvent implements Listener {
         if (!shlkm.isInventoryShulker(p.getInventory().getItemInMainHand(), e.getView().getTitle())) {
             return;
         }
+        e.setCancelled(true);
 
     }
 }
